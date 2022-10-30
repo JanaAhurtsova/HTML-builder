c6ca66a57f30974a87a2a69c472fa77ac1fa13ec
Jana Ahurtsova <byjankaaa@gmail.com>
2022-10-30 21:18:38 +0300

    feat complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+const src = path.resolve(__dirname, 'files');
+const dest = path.resolve(__dirname, 'filesCopy');
 
+async function copyFolder(src, dest) {
+  const files = await fs.readdir(src, { withFileTypes: true }, (err) => {
+    if(err) throw err;
+  });
+
+  await fs.rm(dest, { recursive: true, force: true }, (err) => { if(err) throw err});
+
+  await fs.mkdir(dest, { recursive: true }, (err) => { if(err) throw err});
+
+  for (let file of files) {
+    const srcFilePath = path.resolve(src, file.name);
+    const destFilePath = path.resolve(dest, file.name);
+
+    if (file.isDirectory()) {
+      await copyFolder(srcFilePath, destFilePath);
+    } else {
+      await fs.copyFile(srcFilePath, destFilePath);
+    }
+  }
+}
+copyFolder(src, dest)
